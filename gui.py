e58bbd364c1a263b4d836b1861efa0e2a003c162
zhangta6 <zhangta6@bu.edu>
2023-04-29 17:27:00 -0400

    update

--- gui.py
+++ gui.py
@@ -1,54 +1,55 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QLabel, QLineEdit, QFileDialog
-from doc_feed_analyzer import analyze_document, analyze_feed
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel, QLineEdit, QPushButton
+from models import create_user, get_user_by_email, check_password
 
-class MainWindow(QWidget):
+class RegisterWindow(QWidget):
     def __init__(self):
         super().__init__()
 
         self.init_ui()
 
     def init_ui(self):
-        self.setWindowTitle("Document and Feed Analyzer")
+        self.setWindowTitle("Register")
 
         layout = QVBoxLayout()
 
-        self.upload_button = QPushButton("Upload PDF File")
-        self.upload_button.clicked.connect(self.upload_pdf)
-        layout.addWidget(self.upload_button)
+        self.username_input = QLineEdit()
+        layout.addWidget(QLabel("Username:"))
+        layout.addWidget(self.username_input)
 
-        self.feed_input = QLineEdit()
-        layout.addWidget(QLabel("Feed URL:"))
-        layout.addWidget(self.feed_input)
+        self.email_input = QLineEdit()
+        layout.addWidget(QLabel("Email:"))
+        layout.addWidget(self.email_input)
 
-        self.analyze_button = QPushButton("Analyze")
-        self.analyze_button.clicked.connect(self.analyze)
-        layout.addWidget(self.analyze_button)
+        self.password_input = QLineEdit()
+        self.password_input.setEchoMode(QLineEdit.Password)
+        layout.addWidget(QLabel("Password:"))
+        layout.addWidget(self.password_input)
+
+        self.register_button = QPushButton("Register")
+        self.register_button.clicked.connect(self.register)
+        layout.addWidget(self.register_button)
 
         self.setLayout(layout)
 
-    def upload_pdf(self):
-        options = QFileDialog.Options()
-        file_name, _ = QFileDialog.getOpenFileName(self, "Open PDF File", "", "PDF Files (*.pdf)", options=options)
-        if file_name:
-            self.pdf_file = file_name
+    def register(self):
+        username = self.username_input.text()
+        email = self.email_input.text()
+        password = self.password_input.text()
 
-    def analyze(self):
-        if hasattr(self, "pdf_file"):
-            document_analysis = analyze_document(self.pdf_file)
-            print("Document Analysis:")
-            print(document_analysis)
-
-        feed_url = self.feed_input.text()
-        if feed_url:
-            feed_analysis = analyze_feed(feed_url)
-            print("Feed Analysis:")
-            print(feed_analysis)
+        if not get_user_by_email(email):
+            create_user(username, email, password)
+            self.close()
+        else:
+            self.username_input.clear()
+            self.email_input.clear()
+            self.password_input.clear()
+            self.email_input.setPlaceholderText("Email already exists")
 
 def main():
     app = QApplication(sys.argv)
-    main_window = MainWindow()
-    main_window.show()
+    register_window = RegisterWindow()
+    register_window.show()
     sys.exit(app.exec_())
 
 if __name__ == "__main__":
